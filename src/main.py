fdcd49116eda06c1d693deb82f233334f7acb5ae
Alan Morales <alan_jesus99@hotmail.com>
2020-11-14 16:25:33 -0600

    runnin tests in threads

--- src/main.py
+++ src/main.py
@@ -1,25 +1,41 @@
+import threading
 from selenium import webdriver
 
 DRIVER_PATH = "C:\chromedriver.exe"
-driver = webdriver.Chrome(DRIVER_PATH)
 
-driver.get("http://localhost/Proyecto_OpenPay0/")
-driver.maximize_window()
+def runTest():
+  driver = webdriver.Chrome(DRIVER_PATH)
+  driver.get("http://localhost/Proyecto_OpenPay0/")
+  driver.maximize_window()
 
-name_field = driver.find_element_by_id("name-field")
-name_field.send_keys("alan morales")
+  name_field = driver.find_element_by_id("name-field")
+  name_field.send_keys("alan morales")
 
-card_field = driver.find_element_by_id("card-field")
-card_field.send_keys("4242424242424242")
+  card_field = driver.find_element_by_id("card-field")
+  card_field.send_keys("4242424242424242")
 
-expiration_month_field = driver.find_element_by_id("expitarion-month-field")
-expiration_month_field.send_keys("12")
+  expiration_month_field = driver.find_element_by_id("expitarion-month-field")
+  expiration_month_field.send_keys("12")
 
-expiration_year_field = driver.find_element_by_id("expiration-year-field")
-expiration_year_field.send_keys("21")
+  expiration_year_field = driver.find_element_by_id("expiration-year-field")
+  expiration_year_field.send_keys("21")
 
-cvv_field = driver.find_element_by_id("cvv-field")
-cvv_field.send_keys("123")
+  cvv_field = driver.find_element_by_id("cvv-field")
+  cvv_field.send_keys("123")
 
-send_button = driver.find_element_by_id("btn-enviar")
-send_button.click()
+  send_button = driver.find_element_by_id("btn-enviar")
+  send_button.click()
+
+  # wait = WebDriverWait(driver, 30, poll_frequency=2, ignored_exceptions=[])
+  # transaction_status_element = wait.until(EC.pre)
+  driver.implicitly_wait(30)
+  transaction_status_element = driver.find_element_by_id("transaction-status")
+  print(transaction_status_element.get_attribute("class"))
+
+
+number_of_tests = 4
+
+for i in range(number_of_tests):
+  test_thread = threading.Thread(target=runTest)
+  test_thread.start()
+
